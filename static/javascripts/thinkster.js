fc44f06c36730d1fd86abd4f08be4bf4dbbe6389
Evaldo Bratti <evaldo.bratti@gmail.com>
2015-06-30 22:54:51 -0300

    register functionality

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,13 @@
-angular
-  .module('thinkster', []);
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster', [
+      'thinkster.routes',
+      'thinkster.authentication',
+      'thinkster.config'
+    ]);
+
+  angular.module('thinkster.routes', ['ngRoute']);
+  angular.module('thinkster.config', [])
+})();
