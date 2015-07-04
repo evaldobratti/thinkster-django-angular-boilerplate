bee421a2b7ad7388ed3c58d60c8d1ecdd6f442b7
Evaldo Bratti <evaldo.bratti@gmail.com>
2015-07-04 13:21:49 -0300

    working on ff

---
+++ static/javascripts/layout/controllers/navbar.controller.js
@@ -0,0 +1,30 @@
+/**
+ * Created by evaldo on 03/07/15.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster.layout.controllers')
+    .controller('NavbarController', NavbarController);
+
+  NavbarController.$inject = ['$scope', 'Authentication'];
+
+  /**
+  * @namespace NavbarController
+  */
+  function NavbarController($scope, Authentication) {
+    var vm = this;
+
+    vm.logout = logout;
+
+    /**
+    * @name logout
+    * @desc Log the user out
+    * @memberOf thinkster.layout.controllers.NavbarController
+    */
+    function logout() {
+      Authentication.logout();
+    }
+  }
+})();
